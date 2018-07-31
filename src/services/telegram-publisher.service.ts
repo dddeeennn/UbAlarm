664bf1b647bbaa347093ea051144b44083611298
Dolper <demyan.golyshev@gmail.com>
2018-07-31 23:47:25 +0300

    added proxy

--- src/services/telegram-publisher.service.ts
+++ src/services/telegram-publisher.service.ts
@@ -1,11 +1,15 @@
 var TelegramBot = require('node-telegram-bot-api');
 const token = '626947856:AAEGbrVEdMv9ob-v6N_3PD8rzPsuC5WqXUA';
-var bot = new TelegramBot(token, {polling: true});
 var Config = require('./../config.json');
+const bot = new TelegramBot(token, {
+  polling: true,
+  request: {
+    proxy: Config.proxyUrl,
+  },
+});
 
 export class TelegramPublisher {
     public sendMsg(text): void{
-        // send a message to the chat acknowledging receipt of their message
         bot.sendMessage(Config.chatIdForNotification, text);
     }
 }
